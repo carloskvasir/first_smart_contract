68fd7ac68e8d9c4125a428ca4d3fb909958edf01
Carlos Lima <carlos.lima@reclameaqui.com.br>
2022-01-02 00:03:28 -0300

    add symbol,decimals and supply

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ If you want to learn more, this is the link for the tutorial
 [Learn Ethereum Programming in 1 Hour: Solidity, ERC-20 & Uniswap](https://www.youtube.com/watch?v=XyoFT4QkoL4&ab_channel=DappUniversity)
 
 - [x] Return the name coin: Carlos Kvasir Coin 
-
+- [x] Add Symbol, decimals, supply.
--- Token.sol
+++ Token.sol
@@ -1,6 +1,9 @@
-// SPDX-License-Idenfier: MPL-2.0
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity ^0.8.11;
 
 contract Token {
     string public name = "Carlos Kvasir Coin";
+    string public symbol = "CKR";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 100000000;
 }
